60af15e848f1c742fb0f7568039677de352115fd
eb110 <wfigura@op.pl>
2022-07-16 12:31:17 +0100

    init

---
+++ src/main/java/com/figura/speedway/SpeedwayApplication.java
@@ -0,0 +1,13 @@
+package com.figura.speedway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/figura/speedway/SpeedwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.figura.speedway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
